85bbaa7a3e7b7fb7900bf09ce132819751a93f08
ssenr <duranr@protonmail.com>
2023-12-17 20:39:21 -0500

    Fixed leak properly seperated condition from for

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 bool compare_network_dev(char* name, char* err_buf)
 {
@@ -15,14 +16,14 @@ bool compare_network_dev(char* name, char* err_buf)
     exit(1);
   }
 
+  bool cond = false;
   for (dev=all_dev; dev != NULL; dev = dev->next) {
-    printf("%s \n", dev->name);
-    if ((dev->name) == name) {
-      return true;
+    if (strcmp(dev->name, name) == 0) {
+      cond = true;
     }
   }
   pcap_freealldevs(all_dev);
-  return false;
+  return cond;
 }
 
 int main(int argc, char* argv[])
@@ -31,7 +32,7 @@ int main(int argc, char* argv[])
   char* netwrk_dev = argv[1];
   
   if (compare_network_dev(netwrk_dev, err_buf)) {
-    printf("Success");
+    printf("Success \n");
     return 0;
   }
   return 1;
