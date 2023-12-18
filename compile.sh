09d0230a73ded64b93c61ba69e57095237916ab7
ssenr <duranr@protonmail.com>
2023-12-17 20:18:38 -0500

    Memory Leak

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -Wall -o main main.c -lpcap -lcurl
---
+++ main.c
@@ -0,0 +1,37 @@
+#include <pcap/pcap.h>
+#include <pcap.h>
+#include <curl/curl.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool compare_network_dev(char* name, char* err_buf)
+{
+  pcap_if_t* all_dev;
+  pcap_if_t* dev;
+
+  if (pcap_findalldevs(&all_dev, err_buf) == PCAP_ERROR) {
+    fprintf(stderr, "Error in pcap_findalldevs(): %s \n", err_buf);
+    exit(1);
+  }
+
+  for (dev=all_dev; dev != NULL; dev = dev->next) {
+    printf("%s \n", dev->name);
+    if ((dev->name) == name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int main(int argc, char* argv[])
+{
+  char err_buf[PCAP_ERRBUF_SIZE];
+  char* netwrk_dev = argv[1];
+  
+  if (compare_network_dev(netwrk_dev, err_buf)) {
+    printf("Success");
+    return 0;
+  }
+  return 1;
+}
